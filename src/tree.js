4067337082aad8cf7896a85629e64d114a58403c
root <root@Laptop-Ehsan>
2024-02-10 16:00:57 +0330

    Basic setup

---
+++ src/tree.js
@@ -0,0 +1,14 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor(aray) {
+    this.initArray = Array;
+    this.root = null;
+  }
+}
