8f62911562da73a80f7dcba186c7b344440891fe
root <root@Laptop-Ehsan>
2024-02-11 15:21:48 +0330

    Insert function

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import Tree from "./tree.js";
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+let tree = new Tree([2, 3, 7, 5, 4]);
+
+tree.insert(9);
+prettyPrint(tree.root);
--- src/tree.js
+++ src/tree.js
@@ -6,9 +6,49 @@ class Node {
   }
 }
 
-class Tree {
-  constructor(aray) {
-    this.initArray = Array;
-    this.root = null;
+export default class Tree {
+  constructor(array) {
+    this.sortedArray = [...new Set(array)].sort((a, b) => a - b);
+    this.root = this.buildTree(this.sortedArray, 0, this.sortedArray.length);
+  }
+
+  buildTree(arr, start, end) {
+    const mid = parseInt((start + end) / 2);
+    if (start > end || arr[mid] === undefined) {
+      return null;
+    }
+    const node = new Node(arr[mid]);
+    node.left = this.buildTree(arr, start, mid - 1);
+    node.right = this.buildTree(arr, mid + 1, end);
+    return node;
+  }
+
+  insert(value) {
+    this.root = this.insertRec(this.root, value);
+    // let node = this.root;
+    // let new_node = new Node(value);
+    // while (!node) {
+    //   if (value < node.data) {
+    //     node = node.left;
+    //   } else if (value > node.data) {
+    //     node = node.right;
+    //   } else {
+    //     break;
+    //   }
+    // }
+  }
+
+  insertRec(node, value) {
+    if (!node) {
+      node = new Node(value);
+      return node;
+    }
+    if (value < node.data) {
+      node.left = this.insertRec(node.left, value);
+    } else if (value > node.data) {
+      node.right = this.insertRec(node.right, value);
+    }
+
+    return node;
   }
 }
